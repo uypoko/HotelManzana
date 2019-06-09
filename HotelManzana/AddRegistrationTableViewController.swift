cb34b5f0fd9fafe1bac6b1f88b0330db1ac50bae
Ryan <cungdinhuy@gmail.com>
2019-06-09 19:41:49 +0700

    create UI for adding a new reservation

---
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -0,0 +1,215 @@
+//
+//  AddRegistrationTableViewController.swift
+//  HotelManzana
+//
+//  Created by Uy Cung Dinh on 6/8/19.
+//  Copyright © 2019 Equity. All rights reserved.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController: UITableViewController, SelectRoomTypeTableViewControllerDelegate {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var checkInLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var numberOfAdultsLabel: UILabel!
+    @IBOutlet weak var numberOfAdultsStepper: UIStepper!
+    @IBOutlet weak var numberOfChildrenLabel: UILabel!
+    @IBOutlet weak var numberOfChildrenStepper: UIStepper!
+    
+    @IBOutlet weak var wifiLabel: UILabel!
+    @IBOutlet weak var wifiSwitch: UISwitch!
+    
+    @IBOutlet weak var roomTypeLabel: UILabel!
+    var roomType: RoomType?
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    var isCheckInDatePickerShown: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    var isCheckOutDatePickerIsShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerIsShown
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        updateDateViews()
+        updateNumberOfGuests()
+        updateRoomType()
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        checkInLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row):
+            if isCheckInDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row):
+            if isCheckOutDatePickerIsShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        default:
+            return 44.0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            if isCheckInDatePickerShown && !isCheckOutDatePickerIsShown {
+                isCheckInDatePickerShown = false
+            } else if !isCheckInDatePickerShown && isCheckOutDatePickerIsShown {
+                isCheckOutDatePickerIsShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerIsShown && !isCheckInDatePickerShown {
+                isCheckOutDatePickerIsShown = false
+            } else if !isCheckOutDatePickerIsShown && isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerIsShown = true
+            } else {
+                isCheckOutDatePickerIsShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        default:
+            break
+        }
+    }
+    
+    func updateNumberOfGuests() {
+        numberOfAdultsLabel.text = "\(Int(numberOfAdultsStepper.value))"
+        numberOfChildrenLabel.text = "\(Int(numberOfChildrenStepper.value))"
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfGuests()
+    }
+
+    @IBAction func wifiSwitchChanged(_ sender: Any) {
+        if wifiSwitch.isOn {
+            wifiLabel.text = "$ 10"
+        } else {
+            wifiLabel.text = "$ 0"
+        }
+    }
+    
+    func updateRoomType() {
+        if let roomType = roomType {
+            roomTypeLabel.text = roomType.name
+        } else {
+            roomTypeLabel.text = "Not Set"
+        }
+    }
+    
+    func didSelect(roomType: RoomType) {
+        self.roomType = roomType
+        updateRoomType()
+    }
+    
+    @IBAction func doneBarButtonTapped(_ sender: Any) {
+        
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SelectRoomType" {
+            let selectRoomTypeTableViewController = segue.destination as? SelectRoomTypeTableViewController
+            selectRoomTypeTableViewController?.delegate = self
+            selectRoomTypeTableViewController?.roomType = roomType
+        }
+    }
+
+}
---
+++ HotelManzana/RoomType.swift
@@ -0,0 +1,24 @@
+//
+//  RoomType.swift
+//  HotelManzana
+//
+//  Created by Uy Cung Dinh on 6/8/19.
+//  Copyright © 2019 Equity. All rights reserved.
+//
+
+import Foundation
+struct RoomType: Equatable {
+    var id: Int
+    var name: String
+    var shortName: String
+    var price: Int
+    static var all: [RoomType] {
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+        RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+        RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)]
+    }
+    
+    static func ==(lhs: RoomType, rhs: RoomType) -> Bool{
+        return lhs.id == rhs.id
+    }
+}
